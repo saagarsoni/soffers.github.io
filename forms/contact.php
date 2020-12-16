a3ee69346fdf600b1231f8d470f731d237f2adfc
saagarsoni <saagarsoni3@gmail.com>
2020-12-16 12:11:06 +0530

    fs

--- forms/contact.php
+++ forms/contact.php
@@ -36,6 +36,6 @@
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
-
+  $contact->honeypot = $_POST['first_name'];
   echo $contact->send();
 ?>
